dca770302434615d8218fe8b9c5f7baa64ee2c33
niflan-cyber <editorniflan@gmail.com>
2025-04-26 12:26:26 +0530

    Update space_adventure.py

--- space_adventure.py
+++ space_adventure.py
@@ -79,18 +79,25 @@ class Player(pygame.sprite.Sprite):
             self.power_timer = pygame.time.get_ticks()
 
         # Movement
-        self.speed_x = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.speed_x = 8
-        if keystate[pygame.K_RIGHT]:
-            self.speed_x = -8
+self.speed_x = 0
+keystate = pygame.key.get_pressed()
+if keystate[pygame.K_LEFT]:  # Move Left
+    self.speed_x = -8
+if keystate[pygame.K_RIGHT]:  # Move Right
+    self.speed_x = 8
 
-        self.rect.x += self.speed_x
-        if self.rect.right > SCREEN_WIDTH + 20:
-            self.rect.right = SCREEN_WIDTH + 20
-        if self.rect.left < -20:
-            self.rect.left = -20
+self.rect.x += self.speed_x
+if self.rect.right > SCREEN_WIDTH:
+    self.rect.right = SCREEN_WIDTH
+if self.rect.left < 0:
+    self.rect.left = 0
+
+def shoot(self):
+    if not self.hidden:
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
 
     def shoot(self):
         if not self.hidden:
